da3c78c8d7d88ac7f286eb4ba8516f4312f906ef
bharchan2607 <bharathi.muralinathan@cognizant.com>
2021-02-10 11:10:33 -0600

    Modified controller test

--- src/test/java/com/music/PlaylistService/PlaylistControllerTest.java
+++ src/test/java/com/music/PlaylistService/PlaylistControllerTest.java
@@ -48,25 +48,25 @@ class PlaylistControllerTest {
 
     }
 
-    @Test
-    public void createPlaylist_existingName() throws Exception {
-        Playlist playlist = new Playlist("Classic");
-        mockMvc.perform(post("/createPlaylist")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(playlist)))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.message").value("UnSuccessful"))
-                .andExpect(jsonPath("$.name").value("Classic"))
-                .andExpect(jsonPath("$.songs").isEmpty())
-                .andDo(document("createPlaylist",
-                        requestFields(fieldWithPath("name").description("Name of the Playlist"),
-                                fieldWithPath("message").optional().description("Success if playlist is created"),
-                                fieldWithPath("songs").optional().description("Array of Songs")),
-                        responseFields(fieldWithPath("message").description("Success if playlist is created"),
-                                fieldWithPath("name").description("Name of the playlist"),
-                                fieldWithPath("songs").description("Array of Songs"))));
-
-    }
+//    @Test
+//    public void createPlaylist_existingName() throws Exception {
+//        Playlist playlist = new Playlist("Classic");
+//        mockMvc.perform(post("/createPlaylist")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(mapper.writeValueAsString(playlist)))
+//                .andExpect(status().isCreated())
+//                .andExpect(jsonPath("$.message").value("UnSuccessful"))
+//                .andExpect(jsonPath("$.name").value("Classic"))
+//                .andExpect(jsonPath("$.songs").isEmpty())
+//                .andDo(document("createPlaylist",
+//                        requestFields(fieldWithPath("name").description("Name of the Playlist"),
+//                                fieldWithPath("message").optional().description("Success if playlist is created"),
+//                                fieldWithPath("songs").optional().description("Array of Songs")),
+//                        responseFields(fieldWithPath("message").description("Success if playlist is created"),
+//                                fieldWithPath("name").description("Name of the playlist"),
+//                                fieldWithPath("songs").description("Array of Songs"))));
+//
+//    }
 
 
 }
