ac07ee1c4acfd4c90dcc10c3430cd3a4e0688b1b
bharchan2607 <bharathi.muralinathan@cognizant.com>
2021-02-10 09:16:00 -0600

    Project setup

---
+++ src/test/java/com/music/PlaylistService/PlaylistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.PlaylistService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
